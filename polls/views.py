d02fae59c07d3930858288483f1a0626dff378de
Dare Tijani <oatijani1@gmail.com>
2020-07-17 13:18:18 +0100

    Updated Views

--- polls/views.py
+++ polls/views.py
@@ -29,9 +29,9 @@ def results(request, question_id):
 
 #Vote for a question choice
 def vote(request, question_id):
-    print("here dhhdhhdhdhghd")
-    print(request.POST["choice"])
-    print("here dhhdhhdhdhghd")
+    #print("here dhhdhhdhdhghd")
+    #print(request.POST["choice"])
+    #print("here dhhdhhdhdhghd")
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
